a88f527acfd556ef2bdd86c4d2cfd2d2da9ca13b
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-05 17:40:19 +0800

    Set once  param of event listener to 'true'

--- 25_event_capture_propagation_bubbling_and_once/script.js
+++ 25_event_capture_propagation_bubbling_and_once/script.js
@@ -2,11 +2,12 @@ const divs = document.querySelectorAll('div');
 
 function logText(e) {
   console.log(this.classList.value);
-  e.stopPropagation(); // stops bubbling
+  // e.stopPropagation(); // stops bubbling
 }
 
 // document.body.addEventListener('click', logText);
 
 divs.forEach(div => div.addEventListener('click', logText, {
-  capture: false // captures or 'bubbling' from top-down
+  capture: false, // captures or 'bubbling' from top-down
+  once: true
 }));
