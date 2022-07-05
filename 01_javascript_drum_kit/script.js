4f35360fdc4e5d84babad1611b9c8bb6a4a6cf00
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-05 13:49:35 +0800

    Add condition to check if audio is 'null'

--- 01_javascript_drum_kit/script.js
+++ 01_javascript_drum_kit/script.js
@@ -1,4 +1,6 @@
 window.addEventListener('keydown', function (e) {
   const audio = document.querySelector(`audio[data-code="${e.code}"]`);
+  if(!audio) return; // stops the function from running further
+
   console.log(audio);
 });
