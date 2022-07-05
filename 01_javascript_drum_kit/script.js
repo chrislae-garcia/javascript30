8e327928b6b8ea01ba2be5165f1e4672bee9fd0a
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-05 14:37:33 +0800

    Refactor to separate playSound() function

--- 01_javascript_drum_kit/script.js
+++ 01_javascript_drum_kit/script.js
@@ -1,13 +1,13 @@
-window.addEventListener('keydown', function (e) {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-code="${e.code}"]`);
   const key = document.querySelector(`.key[data-code="${e.code}"]`);
+  
   if(!audio) return; // stops the function from running further
   audio.currentTime = 0;
   audio.play();
   
-  console.log(key);
   key.classList.add('playing');
-});
+}
 
 function removeTransition(e) {
   if (e.propertyName != 'transform') return; // skip it if it is not a transform
@@ -16,3 +16,5 @@ function removeTransition(e) {
 
 const key = document.querySelectorAll('.key');
 key.forEach(key => key.addEventListener('transitionend', removeTransition));
+  
+window.addEventListener('keydown', playSound);
