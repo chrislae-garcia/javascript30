2647eb083b76e14238afcd7a201fcaccc75c306d
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-05 17:34:42 +0800

    Set capture param of event listener to 'true'

--- 25_event_capture_propagation_bubbling_and_once/script.js
+++ 25_event_capture_propagation_bubbling_and_once/script.js
@@ -4,6 +4,8 @@ function logText(e) {
   console.log(this.classList.value);
 }
 
-document.body.addEventListener('click', logText);
+// document.body.addEventListener('click', logText);
 
-divs.forEach(div => div.addEventListener('click', logText));
+divs.forEach(div => div.addEventListener('click', logText, {
+  capture: true // captures or 'bubbling' from top-down
+}));
