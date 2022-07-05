7252bd0a1d2bb27cb9abc97150238fd2b4aa9a10
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-05 14:01:02 +0800

    Add play() to audio

--- 01_javascript_drum_kit/script.js
+++ 01_javascript_drum_kit/script.js
@@ -1,6 +1,7 @@
 window.addEventListener('keydown', function (e) {
   const audio = document.querySelector(`audio[data-code="${e.code}"]`);
   if(!audio) return; // stops the function from running further
-
+  audio.currentTime = 0;
+  audio.play();
   console.log(audio);
 });
