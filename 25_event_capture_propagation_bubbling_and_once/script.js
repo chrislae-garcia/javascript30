f59ab4116128a13906ab08a4900584cab1e948c1
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-05 17:38:16 +0800

    Add .stopPropagation() method to event

--- 25_event_capture_propagation_bubbling_and_once/script.js
+++ 25_event_capture_propagation_bubbling_and_once/script.js
@@ -2,10 +2,11 @@ const divs = document.querySelectorAll('div');
 
 function logText(e) {
   console.log(this.classList.value);
+  e.stopPropagation(); // stops bubbling
 }
 
 // document.body.addEventListener('click', logText);
 
 divs.forEach(div => div.addEventListener('click', logText, {
-  capture: true // captures or 'bubbling' from top-down
+  capture: false // captures or 'bubbling' from top-down
 }));
