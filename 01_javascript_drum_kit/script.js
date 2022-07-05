f6a5828affa4dfbea1fa7157707dcfd4d35dce22
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-05 14:18:38 +0800

    Add .playing to <key>

--- 01_javascript_drum_kit/script.js
+++ 01_javascript_drum_kit/script.js
@@ -1,7 +1,10 @@
 window.addEventListener('keydown', function (e) {
   const audio = document.querySelector(`audio[data-code="${e.code}"]`);
+  const key = document.querySelector(`.key[data-code="${e.code}"]`);
   if(!audio) return; // stops the function from running further
   audio.currentTime = 0;
   audio.play();
-  console.log(audio);
+  
+  console.log(key);
+  key.classList.add('playing');
 });
