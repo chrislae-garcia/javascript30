669725ebd00d3d3d8ed4c6250abfc190855e322e
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-05 17:44:16 +0800

    Set <button> to be clicked 'once'

--- 25_event_capture_propagation_bubbling_and_once/script.js
+++ 25_event_capture_propagation_bubbling_and_once/script.js
@@ -1,4 +1,5 @@
 const divs = document.querySelectorAll('div');
+const button  = document.querySelector('button');
 
 function logText(e) {
   console.log(this.classList.value);
@@ -11,3 +12,9 @@ divs.forEach(div => div.addEventListener('click', logText, {
   capture: false, // captures or 'bubbling' from top-down
   once: true
 }));
+
+button.addEventListener('click', () => {
+  console.log('Click!');
+}, {
+  once: true
+});
